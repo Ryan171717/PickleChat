315289aae220d7d5499fa3335b2e46b1095bc9c8
Ryan171717 <47127375+Ryan171717@users.noreply.github.com>
2019-05-31 07:02:50 -0400

    some login stuff

--- PickleChatServer_UserClass.py
+++ PickleChatServer_UserClass.py
@@ -8,8 +8,8 @@ class User:
         self.update_log(username, password, ip)
         self.message = self.assign_message(username)
     def update_log(self, username, password, ip):
-        u_p_log = open("UserPassword_log.txt", 'a+')
-        u_i_log = open("UserIp_log.txt", 'a+')
+        u_p_log = open("UserPassword_log.txt", 'w+')
+        u_i_log = open("UserIp_log.txt", 'w+')
         userpassword = [username, password]
         for word in userpassword:
             u_p_log.write(word)
@@ -31,15 +31,6 @@ class User:
     def assign_message(self, name):
         messages = ['hello world', 'how are you', 'it tastes like a bologna ball', 'doesnt taste like much', 'that was the plan', 'welp that didnt work']
         return random.choice(messages)
-    
-
-
-        
-host_name = socket.gethostname()
-ip = socket.gethostbyname(host_name)
-username = input("Enter username\n")
-password = input("Enter password\n")
-user1 = User(username, password, str(ip))
 
 pass_dict = {}
 with open("UserPassword_log.txt") as f:
@@ -47,41 +38,40 @@ with open("UserPassword_log.txt") as f:
        (key, val) = line.split()
        pass_dict[key] = val
 
-def login(pass_dict''', user1'''):
+def login(pass_dict):
     while True:
         name = input("Enter name\n")
         password= input("Enter password\n")
+        print(pass_dict)
         if name in pass_dict:
             if pass_dict[name] == password:
                 print("Access Granted")
-                user1.print_ip(name)
-                print(user1.message)
+                User.print_ip(name, name)
+                print(getattr(User.message(name)))
                 break
             else:
-                print("Try again")
+                print("Try again1")
         else:
-            print("Try again")
+            print("Try again2")
 
-
+global user_dict
+user_dict = {}
 def create_user():
     host_name = socket.gethostname()
     ip = socket.gethostbyname(host_name)
     username = input("Enter username:\n")
+    name = username
     password = input("Enter password\n")
-    exec('%s = User % (username, (username, password, ip)))
-
-    
-    
+    user_dict[name] = User(username,password,ip)
+    print(user_dict[name])
     
 while True:
-    selection = input("Would you like to create a new user(y) or login to to a pre-existing account(n).  To exit enter 'e'? (y/n/e)\n")
+    selection = input("To login to to a pre-existing account(n).  To exit enter 'e'? (y/n/e)\n")
     if selection == 'y':
         create_user()
     if selection == 'n':
          login(pass_dict)
     if selection == 'e':
          break
-    else:
+    if selection != 'y' and 'n' and 'e':
          print('Try a different key')
-            
-    
