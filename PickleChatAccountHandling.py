c15f8fcb8d700ad63f05348db2c9d348a27a6edb
Ryan171717 <47127375+Ryan171717@users.noreply.github.com>
2019-06-23 12:49:39 -0400

    new user attribute access patch

--- PickleChatAccountHandling.py
+++ PickleChatAccountHandling.py
@@ -5,19 +5,24 @@ def user_num_update():
     return(user_num)
 
 def GetUsers(user_dict, AttrList, user_num):
+    user_dict = {}
+    user_num = 0
+    AttrList = []
     with open('UserInfo.txt') as f:
+        AttrList = []
         for line in f:
-            key, val = line.split()
-            user_dict[key] = val
-            addAttrs(key, user_num)
+            name, val = line.split()
+            user_dict.update({name: val})
+            addAttrs(name, user_num, user_dict)
             user_num += 1
-def addAttrs(key, user_num):
-    attrs = user_dict[key].split('_')
+        return user_dict
+def addAttrs(name, user_num, user_dict):
+    attrs = user_dict[name].split('_')
     tempAttrList = []
     for attr in attrs:
         tempAttrList.append(attr)
     AttrList.append(tempAttrList)
-    user_dict[key] = user_num
+    user_dict[name] = user_num
     user_num+=1
                         
         
@@ -28,23 +33,31 @@ class User:
         self.username = username
         self.ip = ip
         self.message = self.assign_message()
-        self.update_log(self.username, self.password, self.ip, self.message, user_num)
-
+        self.fmessage = self.update_log(self.username, self.password, self.ip, self.message, user_num)
+        AttrList = []
     def update_log(self, username, password, ip, message, user_num):
-        user_dict.update({username: user_num})
         with open("UserInfo.txt", 'a+') as f:
-            f.write(username)
+            f.write(self.username)
             f.write(' ')
-            f_message = password+'_'+ip+'_'+message
-            f.write(f_message)
+            self.fmessage = self.password+'_'+self.ip+'_'+self.message
+            f.write(self.fmessage)
             f.write('\n')
             user_num += 1
-            GetUsers(user_dict, AttrList, user_num)
-            
+            AttrList = []
+            self.add_user(self.username, self.fmessage)
+            return self.fmessage
     def assign_message(self):
         messages =  ['hello.world', 'how.are.you', 'it.tastes.like.a.bologna.ball', 'doesnt.taste.like.much',
                     'that.was.the.plan', 'welp.that.didnt.work', 'only.one.way.to.find.out']
         return random.choice(messages)
+    def add_user(self, username, fmessage):
+        user_dict.update({self.username: self.fmessage})
+        tempAttrList = []
+        for attr in user_dict[username].split('_'):
+            tempAttrList.append(attr)
+        AttrList.append(tempAttrList)
+        user_dict[self.username] = len(user_dict)-1
+    
 def printAttribute(attr, attr_num):
 
     if attr_num == 2:
@@ -56,7 +69,7 @@ def printAttribute(attr, attr_num):
                 m+= ' '
         return m
                 
-    if attr_num == 0 or 1:
+    else:
         return attr
     
 def create_user():
@@ -77,9 +90,12 @@ def create_user():
                             print("Make sure you type the same password")
                 ip = socket.gethostbyname(socket.gethostname())
                 User(username, password, ip, user_num)
+                AttrList = []
+                
             if new == 'n':
                 return
 def accessAttribute():
+        print(AttrList, '\n', user_dict)
         attr = input("What attribute would you like to find? (ip, password, message)\n")
         if attr != 'message':
             if attr == 'ip':
@@ -91,7 +107,8 @@ def accessAttribute():
         if attr == 'message':
             attr_num = 2
             name =input("Enter username of the user you would like to find the attribute for:\n")
-            print(printAttribute(AttrList[user_dict[name]][attr_num], attr_num))
+        print(printAttribute(AttrList[user_dict[name]][attr_num], attr_num))
+        print(AttrList, '\n', user_dict)
 def main():
 
     global user_dict
@@ -101,15 +118,18 @@ def main():
     global user_num
     user_num = 0
     
-    GetUsers(user_dict, AttrList, user_num)
+    user_dict = GetUsers(user_dict, AttrList, user_num)
 
     print(user_dict)
+    print(AttrList)
     while True:
         choice = input("Would you like to create a new user or access an attribute? (new/access)\n")
         if choice == 'new':
             create_user()
         if choice == 'access':
             accessAttribute()
+        if choice =='break':
+            break
 
     
 main()
