8b386e4990a9982454ddad406fbb73cce4b0b319
Ryan171717 <47127375+Ryan171717@users.noreply.github.com>
2019-06-24 11:46:28 -0400

    fully oo user class

--- PickleChatAccountHandling.py
+++ PickleChatAccountHandling.py
@@ -1,156 +1,91 @@
 import socket, random
-def user_num_update():
-    with open("UserNum.txt") as f:
-        user_num= f
-    return(user_num)
-
-def GetUsers(user_dict, AttrList, user_num):
-    user_dict = {}
-    user_num = 0
-    AttrList = []
-    with open('UserInfo.txt') as f:
-        AttrList = []
-        for line in f:
-            name, val = line.split()
-            user_dict.update({name: val})
-            addAttrs(name, user_num, user_dict)
-            user_num += 1
-        return user_dict
-def addAttrs(name, user_num, user_dict):
-    attrs = user_dict[name].split('_')
-    tempAttrList = []
-    for attr in attrs:
-        tempAttrList.append(attr)
-    AttrList.append(tempAttrList)
-    user_dict[name] = user_num
-    user_num+=1
-                        
-        
 
 class User:
-    def __init__(self, username, password, ip, user_num):
+    def __init__(self, username, password, ip):
         self.password = password
         self.username = username
         self.ip = ip
-        self.message = self.assign_message()
-        self.fmessage = self.update_log(self.username, self.password, self.ip, self.message, user_num)
-        AttrList = []
-    def update_log(self, username, password, ip, message, user_num):
-        with open("UserInfo.txt", 'a+') as f:
-            f.write(self.username)
-            f.write(' ')
-            self.fmessage = self.password+'_'+self.ip+'_'+self.message
-            f.write(self.fmessage)
-            f.write('\n')
-            user_num += 1
-            AttrList = []
-            self.add_user(self.username, self.fmessage)
-            return self.fmessage
-    def assign_message(self):
-        messages =  ['hello.world', 'how.are.you', 'it.tastes.like.a.bologna.ball', 'doesnt.taste.like.much',
-                    'that.was.the.plan', 'welp.that.didnt.work', 'only.one.way.to.find.out']
-        return random.choice(messages)
-    def add_user(self, username, fmessage):
-        user_dict.update({self.username: self.fmessage})
-        tempAttrList = []
-        for attr in user_dict[username].split('_'):
-            tempAttrList.append(attr)
-        AttrList.append(tempAttrList)
-        user_dict[self.username] = len(user_dict)-1
-    def changeMessage(self, new_message, username):
-        self.message = new_message
-        AttrList[user_dict[self.username]][2] = self.message
-    def changePassword(self, new_password, username):
-        self.password == new_password
-        AttrList[user_dict[self.username]][0] = self.password
+        user_dict.update({username:username})
+        self.message = self.assignMessage()
+        with open("Users.txt", 'a+') as f:
+            f.write('{}{}'.format(self.username, '\n'))
+        self.createFile(self.username, self.password, self.ip, self.message)
 
-
-def printAttribute(attr, attr_num):
-
-    if attr_num == 2:
-        m = ''
-        for letter in attr:
-            if letter != '.':
-                m+= letter
-            if letter == '.':
-                m+= ' '
-        return m
-                
-    else:
-        return attr
-    
-def create_user():
-        while True:
-            new = input("Would you like to create a new user: (y/n)")
-            if new == 'y':
-                username = ' '
-                while ' ' in username:
-                    username = input("Enter username\n")
-                    if ' ' in username:
-                        print('try again. use only letters and characters other than {} and {}'.format("'_'", 'spaces'))
-                while True:
-                        password = input("Enter password\n")
-                        confirmation = input("Enter password again\n")
-                        if password == confirmation:
-                            break
-                        else:
-                            print("Make sure you type the same password")
-                ip = socket.gethostbyname(socket.gethostname())
-                User(username, password, ip, user_num)
-                AttrList = []
-                
-            if new == 'n':
-                return
-def accessAttribute():
-        print(AttrList, '\n', user_dict)
-        attr = input("What attribute would you like to find? (ip, password, message)\n")
-        if attr != 'message':
-            if attr == 'ip':
-                attr_num = 1
-            if attr == 'password':
-                attr_num = 0
+    def createFile(self, username, password, ip, message):
+        with open("{}_UserFile.txt".format(username), 'w+') as f:
+            f.write("{} {} {} {}".format(username, password, ip, message))
+    def assignMessage(self):
+        defaultMessages = ['hello world', 'how are you', 'it tastes like a bologna ball',
+                          'doesnt taste like much','that was the plan', 'welp that didnt work',
+                          'only one way to find out']
+        return random.choice(defaultMessages)
+    def changeMessage(self, new_message):
+        passcode = input("Enter your password:\n")
+        if passcode == self.password:
+            self.message = new_message
+            self.createFile(self.username, self.password, self.ip, self.message)
+    def changePassword(self, password):
+        original = input("Enter original password:\n")
+        if original == self.password:
+            new_password = input("Enter your new password:\n")
+            self.password = new_password
+            self.createFile(self.username, self.password, self.ip, self.message)
+            print("Password change successful")
+    def printAttr(self, name, attr):
         if attr == 'message':
-            attr_num = 2
-        if attr != 'ip':
-            while True:
-                name =input("Enter username of the user you would like to find the attribute for:\n")
-                password = input("Enter your password\n")
-                if password == AttrList[user_dict[name]][0]:
-                    login = True
-                    break
-                else:
-                    print("Password is incorrect. Would you like to exit or try again? (exit/again)")
-                    choice = input()
-                    if choice == 'exit':
-                        login = False
+            passcode = input("Enter your password:\n")
+            if passcode == self.password:
+                print(self.message)
+        elif attr == 'ip':
+            print(user_dict[name].ip)
+        elif attr == 'password':
+            passcode = input("Enter your password:\n")
+            if passcode == self.password:
+                print(self.password)
+def choices():
+     while True:
+            choice = input('Would you like to create a new user? (y/n)\n')
+            if choice == 'y':
+                username = input("Username:\n")
+                while True:
+                    password = input("Password:\n")
+                    confirmation = input("Confirm password:\n")
+                    if password == confirmation:
                         break
-                    if choice == 'again':
-                        continue
-            
-        if login == True or attr == 'ip':
-            print(printAttribute(AttrList[user_dict[name]][attr_num], attr_num))
-            print(AttrList, '\n', user_dict)
+                    print('Make sure both passwords are the same.')
+                ip = socket.gethostbyname(socket.gethostname())
+                User(username, password, ip)
+            if choice == 'n':
+                print(user_dict)
+                choice = input('Would you like to access an attribute?(y/n)\n')
+                if choice == 'y':
+                    User.printAttr(user_dict[input("Username")], input("Which attribute?\n"))
+                if choice == 'n':
+                    choice = input("Would you like to change username or password? (message/password)")
+                    if choice == 'message':
+                        name = user_dict[input("Username")]
+                        User.changeMessage(name, name, input("New message:\n"))
+                    if choice == 'password':
+                        User.changePassword(user_dict[input("Username")], input("New password:\n"))
+                if  choice == 'break':
+                    break
+            if choice == 'break':
+                break
 def main():
-
     global user_dict
-    user_dict = {}
-    global AttrList
-    AttrList = []
-    global user_num
-    user_num = 0
-    
-    user_dict = GetUsers(user_dict, AttrList, user_num)
-
-    print(user_dict)
-    print(AttrList)
-    while True:
-        choice = input("Would you like to create a new user or access an attribute? (new/access)\n")
-        if choice == 'new':
-            create_user()
-        if choice == 'access':
-            accessAttribute()
-        if choice =='break':
-            break
-
-    
+    user_dict ={}
+    try:
+        with open("Users.txt") as f:
+            for line in f:
+                if line:
+                    username = line
+                    with open("{}_UserFile.txt".format(username), 'r+') as u:
+                        if '.' in u:
+                            for line in u:
+                                username, password, ip, message = line.split()
+                                User(username, password, ip, message)
+    except FileNotFoundError:
+       choices()
+    choices()
+                    
 main()
